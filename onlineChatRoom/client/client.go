9f0c15e048912991828fbc9ee33419a98ec738b8
CodeCraftsman <2129905621@qq.com>
2025-10-12 16:15:15 +0800

    Update client.go

--- onlineChatRoom/client/client.go
+++ onlineChatRoom/client/client.go
@@ -1,86 +1,105 @@
 package main
 
 import (
-	"awesomeProject/onlineChatRoom/utils"
 	"bufio"
 	"fmt"
 	"log"
 	"net"
+	"onlineChatRoom/msg"
+	"onlineChatRoom/utils"
 	"os"
 	"strings"
 	"time"
 )
 
-// 处理从服务端接收到的信息
+func screen() {
+	fmt.Println("成功加入聊天室，可以开始聊天了...")
+	fmt.Println("可用命令：")
+	fmt.Println("输入：list 查看在线用户...")
+	fmt.Println("输入：quit 退出聊天室...")
+	fmt.Println("输入：To:+用户名-->+内容 私聊...")
+}
+
+// 处理服务端发来的信息
 func handleServerMessage(conn net.Conn) {
 	reader := bufio.NewReader(conn)
 	for {
-		message, err := utils.ReadMessage(reader)
+		message, err := msg.ReadJsonMessage(reader)
 		if err != nil {
-			log.Println("接收服务器消息时出错...")
+			log.Println("服务端已关闭...")
 			return
 		}
-		msgStr := string(message)
-
-		// 如果是心跳回复 PONG，不作为普通消息打印
-		if msgStr == "PONG" {
-			//fmt.Println("[心跳] 收到服务器响应: PONG")
+		switch message.Type {
+		case msg.MessageHeart:
+			//fmt.Println("接收到pong...")
 			continue
-		}
-
-		// 普通消息
-		fmt.Println(msgStr)
-	}
-}
-
-func sendLoginMessage(conn net.Conn) {
-	reader := bufio.NewReader(os.Stdin)
-	for {
-		fmt.Println("请输入你的网名:")
-		input, err := reader.ReadString('\n')
-		if err != nil {
-			log.Println("读取网名失败...")
-			continue
-		}
-		username := strings.TrimSpace(input)
-		if username == "" {
-			fmt.Println("网名不能为空...")
-			continue
-		}
-		//发送网名到服务器
-		err = utils.SendMessage(conn, []byte(username))
-		if err != nil {
-			log.Println("发送网名失败...")
-			continue
-		}
-		//等待服务器响应
-		response, err := utils.ReadMessage(bufio.NewReader(conn))
-		if err != nil {
-			log.Println("接收服务器响应失败...")
-			continue
-		}
-		respStr := string(response)
-		if respStr == "OK" {
-			return
-		} else {
-			fmt.Println(respStr)
-			continue
+		case msg.MessagePrivate:
+			fmt.Println(message.Sender, "私聊你:", message.Content)
+		default:
+			fmt.Println(message.Content)
 		}
 	}
 }
 
 // 发送心跳包
-func startHeartbeat(conn net.Conn) {
-	ticker := time.NewTicker(10 * time.Second) // 每 10 秒发一次心跳
+func startHeartbeat(username string, conn net.Conn) {
+	ticker := time.NewTicker(10 * time.Second)
 	defer ticker.Stop()
 	for {
 		<-ticker.C
-		err := utils.SendMessage(conn, []byte("PING"))
+		message := &msg.Message{
+			Type:    msg.MessageHeart,
+			Sender:  username,
+			Content: "PING",
+		}
+		err := msg.SendJsonMessage(conn, message)
 		if err != nil {
-			log.Println("发送心跳失败:", err)
+			log.Println("startHeartbeat发送心跳结构体失败:", err)
 			return
 		}
-		//fmt.Println("[心跳] 已发送 PING")
+	}
+}
+
+// 键盘输入处理
+func ReadInput() (string, error) {
+	reader := bufio.NewReader(os.Stdin)
+	input, err := reader.ReadString('\n')
+	if err != nil {
+		return "", err
+	}
+	message := strings.TrimSpace(input)
+	if message == "" {
+		return "", fmt.Errorf("输入格式错误，输入不能为空")
+	}
+	return message, nil
+}
+
+// 验证用户名能否登录
+func sendLoginMessage(conn net.Conn) *msg.Message {
+	reader := bufio.NewReader(conn)
+	for {
+		fmt.Println("请输入你的网名:")
+		username, err := ReadInput()
+		if err != nil {
+			log.Println("sendLoginMessage读取网名失败...")
+		}
+		loginMes := &msg.Message{Type: msg.MessageJoin, Sender: username}
+		err = msg.SendJsonMessage(conn, loginMes)
+		if err != nil {
+			log.Println("sendLoginMessage发送网名失败...")
+			continue
+		}
+		response, err := msg.ReadJsonMessage(reader)
+		if err != nil {
+			log.Println("sendLoginMessage接收服务器响应失败...")
+			continue
+		}
+		if response.Content == "OK" {
+			return loginMes
+		} else {
+			fmt.Println(response.Content)
+			continue
+		}
 	}
 }
 
@@ -89,46 +108,48 @@ func main() {
 	if err != nil {
 		log.Fatal("连接服务器出错...", err)
 	}
-	defer func(conn net.Conn) {
-		err := conn.Close()
-		if err != nil {
-			log.Println("conn 退出出错...", err)
-		}
-	}(conn) //延迟关闭
+	defer utils.CloseConn(conn, "客户端")
 
 	fmt.Println("-------------欢迎来到网络聊天室-------------")
-	sendLoginMessage(conn)       //输入网名
-	go handleServerMessage(conn) //开启一个协程来处理服务端的信息
-	go startHeartbeat(conn)      // 开启心跳检测
-
-	fmt.Println("成功加入聊天室，可以开始聊天了...")
-	fmt.Println("可用命令：")
-	fmt.Println("输入：list 查看在线用户...")
-	fmt.Println("输入：quit 退出聊天室...")
-	fmt.Println("输入：To:+用户名-->+内容 私聊...")
-
+	loginMes := sendLoginMessage(conn)       //输入网名
+	go handleServerMessage(conn)             //开启一个协程来处理服务端的信息
+	go startHeartbeat(loginMes.Sender, conn) //开启心跳检测
+	screen()
 	for {
-		reader := bufio.NewReader(os.Stdin)
-		message, err := reader.ReadString('\n')
+		content, err := ReadInput()
 		if err != nil {
-			log.Println("读取用户输入错误...")
-		}
-		message = strings.TrimSpace(message)
-		if message == "" {
-			fmt.Println("输入不能为空..")
+			log.Println("main输入失败...")
 			continue
 		}
-		if message == "quit" {
+		if content == "quit" {
 			fmt.Println("退出成功...")
-			err := utils.SendMessage(conn, []byte("quit"))
-			if err != nil {
-				log.Fatal("退出连接时出错...")
-			}
 			break
 		}
-		err = utils.SendMessage(conn, []byte(message))
-		if err != nil {
-			log.Fatal("发送信息失败...")
+		if content == "list" {
+			err := msg.SendJsonMessage(conn, &msg.Message{Type: msg.MessageList, Sender: loginMes.Sender, Content: "quit"})
+			if err != nil {
+				log.Println("main发送list失败...")
+				continue
+			}
+			continue
+		}
+		if strings.HasPrefix(content, "To:") {
+			parts := strings.SplitN(content, "-->", 2)
+			if len(parts) < 2 {
+				fmt.Println("格式错误，应为 To:用户名-->内容")
+				continue
+			}
+			target := strings.TrimPrefix(parts[0], "To:")
+			err := msg.SendJsonMessage(conn, &msg.Message{Type: msg.MessagePrivate, Sender: loginMes.Sender, Receiver: target, Content: parts[1]})
+			if err != nil {
+				log.Println("main发送私聊失败...")
+				continue
+			}
+			continue
+		}
+		r := msg.SendJsonMessage(conn, &msg.Message{Type: msg.MessageChat, Sender: loginMes.Sender, Content: content})
+		if r != nil {
+			log.Println("main发送普通消息失败...")
 		}
 	}
 }
