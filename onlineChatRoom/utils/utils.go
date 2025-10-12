72f0eddfcbde851f1cf43b3b24c33822706b534d
CodeCraftsman <2129905621@qq.com>
2025-10-12 16:15:29 +0800

    Update utils.go

--- onlineChatRoom/utils/utils.go
+++ onlineChatRoom/utils/utils.go
@@ -3,6 +3,7 @@ package utils
 import (
 	"bufio"
 	"encoding/binary"
+	"errors"
 	"fmt"
 	"io"
 	"log"
@@ -15,40 +16,50 @@ const maxMessageLength = 1 << 20 // 1MB，最大消息长度限制
 func SendMessage(conn net.Conn, message []byte) error {
 	length := uint32(len(message)) // 消息长度
 	if length > maxMessageLength {
-		log.Println("消息长度超出限制: ", length)
+		//log.Println("消息长度超出限制: ", length)
 		return fmt.Errorf("message too long")
 	}
 	// 写入消息长度
 	err := binary.Write(conn, binary.BigEndian, length) //将数据以二进制写入conn
 	if err != nil {
-		log.Printf("写入消息长度时出错: %v", err)
-		return err
+		//log.Printf("写入消息长度时出错: %v", err)
+		return fmt.Errorf("binary.Write--写入消息长度时出错")
 	}
 	// 写入消息内容
 	_, err = conn.Write(message)
 	if err != nil {
-		log.Printf("写入消息内容时出错: %v", err)
+		//log.Printf("写入消息内容时出错: %v", err)
+		return fmt.Errorf("conn.Write--写入消息时出错")
 	}
-	return err
+	return nil
 }
 
 // ReadMessage 从连接读取消息
 func ReadMessage(reader *bufio.Reader) ([]byte, error) {
-	var length uint32                                     // 消息长度
-	err := binary.Read(reader, binary.BigEndian, &length) //从reader中读取二进制数据并解析为结构化数据，自动填充到length中
+	var length int32
+	err := binary.Read(reader, binary.BigEndian, &length)
 	if err != nil {
+		// 当连接关闭或长度无法读取时视为 EOF（正常断开）
+		if errors.Is(err, io.EOF) || errors.Is(err, io.ErrUnexpectedEOF) {
+			return nil, io.EOF
+		}
 		return nil, err
 	}
-	if length > maxMessageLength {
-		log.Println("消息长度超出限制:", length)
-		return nil, fmt.Errorf("message too long")
-	}
-	// 读取消息内容
-	message := make([]byte, length)
-	_, err = io.ReadFull(reader, message)
+
+	buf := make([]byte, length)
+	_, err = io.ReadFull(reader, buf)
 	if err != nil {
-		log.Printf("读取消息内容时出错: %v", err)
+		if errors.Is(err, io.EOF) || errors.Is(err, io.ErrUnexpectedEOF) {
+			return nil, io.EOF
+		}
 		return nil, err
 	}
-	return message, nil
+	return buf, nil
+}
+
+func CloseConn(conn net.Conn, name string) {
+	err := conn.Close()
+	if err != nil {
+		log.Printf("%s 关闭连接失败： %s \n", name, err)
+	}
 }
